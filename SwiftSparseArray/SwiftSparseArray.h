747be0ac8320bf36d2377b147df46a661b459560
Evan McKee <emckee@gmail.com>
2015-10-10 19:54:42 -0700

    Initial Commit

---
+++ SwiftSparseArray/SwiftSparseArray.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSparseArray.h
+//  SwiftSparseArray
+//
+//  Created by Evan Mckee on 10/10/15.
+//  Copyright © 2015 McKeeMaKer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSparseArray.
+FOUNDATION_EXPORT double SwiftSparseArrayVersionNumber;
+
+//! Project version string for SwiftSparseArray.
+FOUNDATION_EXPORT const unsigned char SwiftSparseArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSparseArray/PublicHeader.h>
+
+
